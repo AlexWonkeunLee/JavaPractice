d5c35bebec71ca194954679894bcdf40d36a423b
AlexWonkeunLee <AlexWonkeunLee@gmail.com>
2020-08-11 18:39:58 -0500

    very basic java stuff

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,27 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+        String name;
+        name = "asdf";
+        System.out.println(name);
+
+        int salary = 50;
+        int monthly = salary * 4;
+        System.out.println(salary);
+        System.out.println(monthly);
+
+        int apples = 44;
+        int oranges = 24;
+        int fruit = apples + oranges;
+        System.out.println(fruit);
+
+        int weeks = 130;
+        double years = weeks / 52.0;
+        System.out.println(years);
+
+        System.out.println("Hello, " + name + " here");
+
+
     }
 }
